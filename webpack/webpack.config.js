baab5827a5811d1cb85af245d5ac58291caf1008
eunkong0-0 <joyjessica@naver.com>
2024-05-28 11:10:10 +0900

    webpack babel

---
+++ webpack/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry : "./script.js",
+  output : {
+    path : __dirname,
+    filename : "build.js",
+  }, 
+  module : {
+    rules : [
+      {
+        test : /\.js$/,
+        use : {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        },
+      }
+    ]
+  }
+};
